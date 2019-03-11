9b708d4030a0af48443ee11e4debbc3df8a5518e
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2019-03-11 21:45:29 +0600

    comment create and show by fronted done

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -3,6 +3,23 @@
 	class Db_object{
 
 
+		public $errors = array();
+		public $upload_errors_array = array(
+
+		UPLOAD_ERR_OK			 => "There is no error",
+		UPLOAD_ERR_INI_SIZE 	 => "The uploaded file exceeds the upload_max_filesize directive in php.ini",
+		UPLOAD_ERR_FORM_SIZE 	 => "The uploaded file exceeds the MAX_FILE_SIZE was specified in the HTML form",
+		UPLOAD_ERR_PARTIAL       => "The uploaded file was only partially uploaded.",
+		UPLOAD_ERR_NO_FILE       => "No file was uploaded.",
+		UPLOAD_ERR_NO_TMP_DIR    => "Missing a temporary folder.",
+		UPLOAD_ERR_CANT_WRITE    => "Failed to write file to disk.",
+		UPLOAD_ERR_EXTENSION     => "A PHP extension stopped the file upload.",
+
+
+		 );
+
+
+
 		
 
 
